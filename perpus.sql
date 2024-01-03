d04ecb0226c5eb496cf23ec35791d8a654352b10
Albert Okario <okarioalbert@gmail.com>
2024-01-03 18:25:38 +0700

    first commit

---
+++ perpus.sql
@@ -0,0 +1,288 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 03, 2024 at 12:12 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_biaya_denda`
+--
+
+CREATE TABLE `tbl_biaya_denda` (
+  `id_biaya_denda` int(11) NOT NULL,
+  `harga_denda` varchar(255) NOT NULL,
+  `stat` varchar(255) NOT NULL,
+  `tgl_tetap` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_biaya_denda`
+--
+
+INSERT INTO `tbl_biaya_denda` (`id_biaya_denda`, `harga_denda`, `stat`, `tgl_tetap`) VALUES
+(9, '10000', 'Aktif', '2024-01-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_buku`
+--
+
+CREATE TABLE `tbl_buku` (
+  `id_buku` int(11) NOT NULL,
+  `buku_id` varchar(255) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_rak` int(11) NOT NULL,
+  `sampul` varchar(255) DEFAULT NULL,
+  `isbn` varchar(255) DEFAULT NULL,
+  `lampiran` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `penerbit` varchar(255) DEFAULT NULL,
+  `pengarang` varchar(255) DEFAULT NULL,
+  `thn_buku` varchar(255) DEFAULT NULL,
+  `isi` text,
+  `jml` int(11) DEFAULT NULL,
+  `tgl_masuk` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_buku`
+--
+
+INSERT INTO `tbl_buku` (`id_buku`, `buku_id`, `id_kategori`, `id_rak`, `sampul`, `isbn`, `lampiran`, `title`, `penerbit`, `pengarang`, `thn_buku`, `isi`, `jml`, `tgl_masuk`) VALUES
+(9, 'BK001', 3, 2, '870765a63ac4641d3f130ee24a87d494.jpg', '0-6812-5863-2', NULL, 'Naruto Vol 1', 'Masashi Kishimoto', 'Masashi Kishimoto', '2000', '<p>VOL1</p>', 12, '2024-01-03 03:29:59'),
+(10, 'BK0010', 3, 2, '523e07941ceec335fad683de115344a8.jpg', '0-9353-7272-5', NULL, 'Naruto Vol 2', 'Masashi Kishimoto', 'Masashi Kishimoto', '2005', '<p>VOL 2</p>', 1, '2024-01-03 03:37:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_denda`
+--
+
+CREATE TABLE `tbl_denda` (
+  `id_denda` int(11) NOT NULL,
+  `pinjam_id` varchar(255) NOT NULL,
+  `denda` varchar(255) NOT NULL,
+  `lama_waktu` int(11) NOT NULL,
+  `tgl_denda` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_denda`
+--
+
+INSERT INTO `tbl_denda` (`id_denda`, `pinjam_id`, `denda`, `lama_waktu`, `tgl_denda`) VALUES
+(6, 'PJ001', '0', 0, '2024-01-03'),
+(7, 'PJ0012', '10000', 1, '2024-01-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(3, 'Manga');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `id_login` int(11) NOT NULL,
+  `anggota_id` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tempat_lahir` varchar(255) NOT NULL,
+  `tgl_lahir` varchar(255) NOT NULL,
+  `jenkel` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `telepon` varchar(25) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `tgl_bergabung` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`id_login`, `anggota_id`, `user`, `pass`, `level`, `nama`, `tempat_lahir`, `tgl_lahir`, `jenkel`, `alamat`, `telepon`, `email`, `tgl_bergabung`, `foto`) VALUES
+(1, 'AG001', 'AlzS19', '202cb962ac59075b964b07152d234b70', 'Admin', 'Albert Okario', 'Tangerang', '2003-10-15', 'Laki-Laki', 'BALI', '081271217847', 'albertokario059@student.unud.id', '2024-01-03', 'user_1704227283.jpeg'),
+(2, 'AG002', 'user', '202cb962ac59075b964b07152d234b70', 'Anggota', 'Albert', 'Tangerang', '2024-01-03', 'Laki-Laki', 'asfaf', '0908103840185', 'asfasf@gmail.com', '2024-01-03', 'user_1704227295.jpg'),
+(6, 'AG003', 'user2', '202cb962ac59075b964b07152d234b70', 'Anggota', 'Cindy', 'Tangerang', '2024-01-03', 'Perempuan', 'adsa', '809809809', 'saofa@gmail.com', '2024-01-03', 'user_1704231620.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pinjam`
+--
+
+CREATE TABLE `tbl_pinjam` (
+  `id_pinjam` int(11) NOT NULL,
+  `pinjam_id` varchar(255) NOT NULL,
+  `anggota_id` varchar(255) NOT NULL,
+  `buku_id` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `tgl_pinjam` varchar(255) NOT NULL,
+  `lama_pinjam` int(11) NOT NULL,
+  `tgl_balik` varchar(255) NOT NULL,
+  `tgl_kembali` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pinjam`
+--
+
+INSERT INTO `tbl_pinjam` (`id_pinjam`, `pinjam_id`, `anggota_id`, `buku_id`, `status`, `tgl_pinjam`, `lama_pinjam`, `tgl_balik`, `tgl_kembali`) VALUES
+(11, 'PJ001', 'AG002', 'BK001', 'Di Kembalikan', '2024-01-03', 7, '2024-01-10', '2024-01-03'),
+(12, 'PJ0012', 'AG002', 'BK001', 'Di Kembalikan', '2024-01-01', 1, '2024-01-02', '2024-01-03'),
+(13, 'PJ0013', 'AG002', 'BK0010', 'Dipinjam', '2024-01-03', 13, '2024-01-16', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_rak`
+--
+
+CREATE TABLE `tbl_rak` (
+  `id_rak` int(11) NOT NULL,
+  `nama_rak` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_rak`
+--
+
+INSERT INTO `tbl_rak` (`id_rak`, `nama_rak`) VALUES
+(2, 'Rak 1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_biaya_denda`
+--
+ALTER TABLE `tbl_biaya_denda`
+  ADD PRIMARY KEY (`id_biaya_denda`);
+
+--
+-- Indexes for table `tbl_buku`
+--
+ALTER TABLE `tbl_buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `tbl_denda`
+--
+ALTER TABLE `tbl_denda`
+  ADD PRIMARY KEY (`id_denda`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indexes for table `tbl_pinjam`
+--
+ALTER TABLE `tbl_pinjam`
+  ADD PRIMARY KEY (`id_pinjam`);
+
+--
+-- Indexes for table `tbl_rak`
+--
+ALTER TABLE `tbl_rak`
+  ADD PRIMARY KEY (`id_rak`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_biaya_denda`
+--
+ALTER TABLE `tbl_biaya_denda`
+  MODIFY `id_biaya_denda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tbl_buku`
+--
+ALTER TABLE `tbl_buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_denda`
+--
+ALTER TABLE `tbl_denda`
+  MODIFY `id_denda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_pinjam`
+--
+ALTER TABLE `tbl_pinjam`
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tbl_rak`
+--
+ALTER TABLE `tbl_rak`
+  MODIFY `id_rak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
